87fb2338fb327f9b6ba4f02a50a4fd305f573721
Akrobate <fedorov.artiom@gmail.com>
2024-06-28 23:27:03 +0200

    Working on light animation class

---
+++ code/CassettePlayerToy/lib/LightAnimation/LightAnimation.cpp
@@ -0,0 +1,5 @@
+#include "LightAnimation.h"
+
+
+LightAnimation::LightAnimation() {
+}
---
+++ code/CassettePlayerToy/lib/LightAnimation/LightAnimation.h
@@ -0,0 +1,23 @@
+#ifndef LightAnimation_h
+#define LightAnimation_h
+
+#include <Arduino.h>
+
+/**
+ * About LightAnimation class
+ */
+
+class LightAnimation {
+    
+    public:
+
+        LightAnimation();
+
+
+    private:
+
+        // const int MEDIAN_SCORE = 553;
+
+};
+
+#endif
