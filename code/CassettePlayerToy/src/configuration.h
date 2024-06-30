7cd7cc41c622d3d9f21809477a17ccee1a7d0d10
Akrobate <fedorov.artiom@gmail.com>
2024-06-30 02:08:39 +0200

    Added configuration for pins

---
+++ code/CassettePlayerToy/src/configuration.h
@@ -0,0 +1,30 @@
+#ifndef configuration_h
+#define configuration_h
+
+#define BUTTON_PREVIOUS  9
+#define BUTTON_NEXT      11
+#define BUTTON_PLAY 10
+
+#define BUTTON_VOLUME_DOWN  8
+#define BUTTON_VOLUME_UP    7
+
+#define RX_PIN 2  // plugged on TX of the DFPlayer
+#define TX_PIN 3  // plugged on RX of the DFPlayer
+
+#define PIXEL_LED_PIN 4
+
+
+#define CNY70_INPUT_0  A2
+#define CNY70_LED_0  12
+
+#define CNY70_INPUT_1  A3
+#define CNY70_LED_1  13
+
+#define CNY70_INPUT_2  A4
+#define CNY70_LED_2  14
+
+#define CNY70_INPUT_3  A5
+#define CNY70_LED_3  15
+
+
+#endif
