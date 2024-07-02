b33799f5416b330e7045a0f353d0ec7e1608f3c0
Akrobate <fedorov.artiom@gmail.com>
2024-07-02 13:39:09 +0200

    Updated main file, finished mp3 business

--- code/CassettePlayerToy/src/main.cpp
+++ code/CassettePlayerToy/src/main.cpp
@@ -4,20 +4,14 @@
 #include <DFRobotDFPlayerMini.h>
 #include <Toggle.h>
 
-#include <FastLED.h>
-
 #include "./configuration.h"
 #include <LightAnimation.h>
-#include <Cny70.h>
-
-
-LightAnimation lightAnimation();
-
-// Pixel led (ws2812b)
-
-CRGB leds[1];
+#include <CassetteReader.h>
 
 void printDetail(uint8_t type, int value);
+void nextTrackInFolder();
+void previousTrackInFolder();
+
 
 SoftwareSerial softSerial(RX_PIN, TX_PIN);
 #define FPSerial softSerial
@@ -29,32 +23,28 @@ Toggle buttonNext(BUTTON_NEXT);
 Toggle buttonPrevious(BUTTON_PREVIOUS);
 
 DFRobotDFPlayerMini myDFPlayer;
-
-boolean is_playing = 0;
-
+bool is_playing = false;
 unsigned int volume = 10;
 
-int cny70_0 = 0;
-int cny70_1 = 0;
-int cny70_2 = 0;
-int cny70_3 = 0;
 
+int current_folder = 0;
+int files_in_folder_count = 0;
+int current_file = 0;
 
-Cny70 cny_1(CNY70_INPUT_0, CNY70_LED_0);
-Cny70 cny_2(CNY70_INPUT_0, CNY70_LED_0);
-Cny70 cny_3(CNY70_INPUT_0, CNY70_LED_0);
+const unsigned int max_volume = 20;
 
-
+CassetteReader * cassette_reader = new CassetteReader();
+LightAnimation * light_animation = new LightAnimation();
 
 void setup() {
+
+    cassette_reader->serial_debug_activate = true;
 
     FPSerial.begin(9600);
     Serial.begin(9600);
 
     Serial.println();
     Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
-
-
 
     if (!myDFPlayer.begin(FPSerial, true, false)) {
         Serial.println(F("Unable to begin"));
@@ -63,46 +53,36 @@ void setup() {
         }
     }
 
-
-    myDFPlayer.volume(0);
-
-    // myDFPlayer.play(1);
+    myDFPlayer.volume(volume);
 
     Serial.println(F("DFPlayer on the air !!! "));
-
-    //delay(5000);
-
-
-    //myDFPlayer.reset();
-
-
-    // CNY70
-    pinMode(CNY70_INPUT_0, INPUT);
-    pinMode(CNY70_LED_0, OUTPUT);
-
-    pinMode(CNY70_INPUT_1, INPUT);
-    pinMode(CNY70_LED_1, OUTPUT);
-
-    pinMode(CNY70_INPUT_2, INPUT);
-    pinMode(CNY70_LED_2, OUTPUT);
-
-    pinMode(CNY70_INPUT_3, INPUT);
-    pinMode(CNY70_LED_3, OUTPUT);
-
-
-    pinMode(PIXEL_LED_PIN, OUTPUT);
-    digitalWrite(PIXEL_LED_PIN, LOW);
-    FastLED.addLeds<NEOPIXEL, PIXEL_LED_PIN>(leds, 1);
-
-    
+    light_animation->triggerAnimationStartDevice();
 }
 
 
 
 void loop() {
 
-    // static unsigned long timer = millis();
+    cassette_reader->update();
+    light_animation->update();
 
+    if (cassette_reader->valueChanged()) {
+        Serial.print("Cassette reader value changed: ");
+        Serial.println(cassette_reader->getIntegerValue());
+        current_folder = cassette_reader->getIntegerValue();
+
+        if (current_folder > 0) {
+            files_in_folder_count = myDFPlayer.readFileCountsInFolder(current_folder);
+            light_animation->triggerAnimationCassetteDetected();
+            current_file = 1;
+            myDFPlayer.playFolder(current_folder, current_file);
+            is_playing = true;
+        } else {
+            light_animation->triggerAnimationNoCassette();
+            myDFPlayer.pause();
+            is_playing = false;
+        }
+    }
 
     buttonVolumeUp.poll();
     buttonVolumeDown.poll();
@@ -112,8 +92,8 @@ void loop() {
 
     if (buttonVolumeUp.onPress()) {
         volume += 1;
-        if (volume > 30) {
-            volume = 30;
+        if (volume > max_volume) {
+            volume = max_volume;
         }
         myDFPlayer.volume(volume);
 
@@ -130,74 +110,133 @@ void loop() {
         }
     }
 
-
-
     if (buttonPlay.onPress()) {
         Serial.println(F("Play"));
+        if (is_playing) {
+            light_animation->triggerAnimationPaused();
+            myDFPlayer.pause();
+            is_playing = false;
+        } else {
+            light_animation->triggerAnimationCassetteDetected();
+            myDFPlayer.start();
+            is_playing = true;
+        }
     }
 
     if (buttonNext.onPress()) {
         Serial.println(F("Next file:"));
+        nextTrackInFolder();
     }
 
     if (buttonPrevious.onPress()) {
         Serial.println(F("Previous file:"));
+        previousTrackInFolder();
     }
 
 
+    if (myDFPlayer.available()) {
+        printDetail(myDFPlayer.readType(), myDFPlayer.read());
 
+        if (DFPlayerPlayFinished == myDFPlayer.readType()) {
+            is_playing = false;
+            nextTrackInFolder();
+        }
 
-    /*
-
-    digitalWrite(CNY70_LED_0, HIGH);
-    delay(100);
-    cny70_0 = analogRead(CNY70_INPUT_0);
-    digitalWrite(CNY70_LED_0, LOW);
-
-    digitalWrite(CNY70_LED_1, HIGH);
-    delay(100);
-    cny70_1 = analogRead(CNY70_INPUT_1);
-    digitalWrite(CNY70_LED_1, LOW);
-
-    digitalWrite(CNY70_LED_2, HIGH);
-    delay(100);
-    cny70_2 = analogRead(CNY70_INPUT_2);
-    digitalWrite(CNY70_LED_2, LOW);
-
-    digitalWrite(CNY70_LED_3, HIGH);
-    delay(100);
-    cny70_3 = analogRead(CNY70_INPUT_3);
-    digitalWrite(CNY70_LED_3, LOW);
-
-    Serial.print("CNY Values: ");
-    Serial.print("CNY0: ");
-    Serial.print(cny70_0);
-    Serial.print(", CNY1: ");
-    Serial.print(cny70_1);
-    Serial.print(", CNY2: ");
-    Serial.print(cny70_2);
-    Serial.print(", CNY3: ");
-    Serial.println(cny70_3);
-
-    */
-
-// delay(1000);
-
-    leds[0] = CRGB::Red;
-    //FastLED.show();
-
-
-    //delay(100);
-
-
-    // leds[0] = CRGB::Green;
-    // FastLED.show();
-
-
-    //if (myDFPlayer.available()) {
-    //    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
-    //}
+        if (FileMismatch == myDFPlayer.readType()) {
+            is_playing = false;
+            light_animation->triggerAnimationUnknownCassette();
+        }
+    }
 
 }
 
 
+void nextTrackInFolder() {
+
+    if (current_file < files_in_folder_count) {
+        current_file += 1;
+    } else {
+        current_file = 1;
+    }
+
+    myDFPlayer.playFolder(current_folder, current_file);
+    is_playing = true;
+
+}
+
+
+void previousTrackInFolder() {
+
+    if (current_file > 1) {
+        current_file -= 1;
+    } else {
+        current_file = files_in_folder_count;
+    }
+
+    myDFPlayer.playFolder(current_folder, current_file);
+    is_playing = true;
+
+}
+
+
+
+void printDetail(uint8_t type, int value) {
+  switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerUSBInserted:
+      Serial.println("USB Inserted!");
+      break;
+    case DFPlayerUSBRemoved:
+      Serial.println("USB Removed!");
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+}
