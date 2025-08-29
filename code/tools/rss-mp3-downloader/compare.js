70c4123c042f7eaa977d3c6e20a762123866dcea
Akrobate <fedorov.artiom@gmail.com>
2025-08-29 23:10:37 +0200

    Added compar script

---
+++ code/tools/rss-mp3-downloader/compare.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const result = JSON.parse(fs.readFileSync(path.join('data', 'result.json'), 'utf8'));
+
+// console.log(result)
+
+const data = {};
+
+result.map(item => item.title_formated).forEach((item) => {
+    data[item] = data[item] ? data[item]++ : 1;
+})
+
+console.log(data);
+
+Object.keys(data).forEach(key => {
+    //console.log(key)
+    if (data[key] > 1) {
+        console.log(key);
+    }
+});
